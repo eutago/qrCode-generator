e30eb58812bf128a812d0b50a74b79c250a65035
Thiago Luiz <thiagoluix21@gmail.com>
2022-11-20 15:14:07 -0300

    projeto qrCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import QRCode from 'react-qr-code';
+import qrcode from 'qrcode';
 import './App.css';
 
 function App() {
+  const [link, setLink] = useState('');
+  const [qrCodeLink, setQrCodeLink] = useState('');
+
+  const handleDownload = (url: string) => {
+    qrcode.toDataURL(
+      url,
+      {
+        width: 600,
+        margin: 3,
+      },
+      (_error, url) => {
+        try {
+          setQrCodeLink(url);
+        } catch (_error) {
+          throw new Error('Ops, algo deu errado!');
+        }
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="title">
+          Qr<span>Code</span>Generator
+        </h1>
       </header>
+      <main>
+        <div className="qrCode-container">
+          <QRCode size={180} value={link}></QRCode>
+        </div>
+        <div className="inputGroup">
+          <input
+            onChange={(event) => {
+              setLink(event.target.value);
+              handleDownload(event.target.value);
+            }}
+            type="text"
+            className="inputWrite"
+            placeholder="Digite seu link aqui..."
+          />
+          <a href={qrCodeLink} download={`qrcode.jpg`}>
+            <button className="downloadQRCode">Baixar QRCode</button>
+          </a>
+        </div>
+      </main>
+      <footer>
+        <p>Created by:</p>
+        <p>@eutago</p>
+      </footer>
     </div>
   );
 }
